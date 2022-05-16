9db117a79e68e82feab725b96c40ae7f80f98f94
mir-hussain <mirhussainmurtaza@gmail.com>
2022-05-16 23:24:10 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='h-screen flex items-center justify-center'>
+      <h1 className='font-extrabold text-transparent text-8xl leading-loose bg-clip-text bg-gradient-to-r from-teal-400 to-purple-400'>
+        Daisy UI Initialized
+      </h1>
     </div>
   );
 }
