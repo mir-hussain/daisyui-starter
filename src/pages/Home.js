0570270895e38d64f570186d7199e42fdae24b32
mir-hussain <mirhussainmurtaza@gmail.com>
2022-05-17 01:38:05 +0600

    cover image added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,8 +3,8 @@ import React from "react";
 const Home = () => {
   return (
     <div className='h-screen flex items-center justify-center'>
-      <h1 className='font-extrabold text-transparent text-2xl leading-loose bg-clip-text bg-gradient-to-r from-teal-400 to-purple-400'>
-        Daisy UI Initialized
+      <h1 className='font-bold text-transparent text-8xl leading-loose bg-clip-text bg-gradient-to-r from-teal-400 to-purple-400'>
+        Daisy UI
       </h1>
     </div>
   );
